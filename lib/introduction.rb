0efa1d639f4ac03e748d43df65fe001a14248c0b
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-12 21:00:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
+def introcution(name)
+  puts "Hi, my name is #{name}."
+end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
